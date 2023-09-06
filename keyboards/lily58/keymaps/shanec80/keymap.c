d4c3336572df1d549d055f94838f9bb5c4106967
carakesa <carakesa@gmail.com>
2023-09-06 09:55:27 -0400

    keymap.c - tweak NUM, FUN, NAV layers

--- keyboards/lily58/keymaps/shanec80/keymap.c
+++ keyboards/lily58/keymaps/shanec80/keymap.c
@@ -64,7 +64,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  [NAV] = LAYOUT(
  _______, _______, _______, _______, _______, _______,                    _______, _______, _______, _______, _______, _______,
     _______, _______, _______, _______, _______, _______,                 KC_CUT , KC_COPY, KC_PSTE, KC_UNDO, _______, _______,
-    _______, _______, KC_LGUI, KC_LALT, KC_LSFT, KC_LCTL,                 KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, _______, _______,
+    _______, _______, KC_LGUI, KC_LALT, KC_LSFT, KC_LCTL,                 KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, KC_END, _______,
     _______, _______, _______, _______, _______, _______, _______, KC_HOME, KC_PGDN, KC_PGUP, KC_END, KC_INS, _______, _______,
                       _______, _______, _______,  _______,      KC_ENT , KC_SPC, KC_BSPC, KC_DEL
 ),
@@ -92,8 +92,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 ),
  [NUM] = LAYOUT(
     _______,  _______, _______, _______, _______, _______,                    _______, _______, _______, _______, _______, QK_BOOT,
- _______,    KC_LBRC, KC_7   , KC_8   , KC_9   , KC_RBRC,        _______, _______, _______, _______, _______, _______,
- _______,   KC_QUOT, KC_4   , KC_5   , KC_6   , KC_EQL ,        _______, _______, KC_RCTL, KC_RSFT, KC_LALT, KC_RGUI,
+ KC_NUM,    KC_LBRC, KC_7   , KC_8   , KC_9   , KC_RBRC,        _______, _______, _______, _______, _______, _______,
+ KC_NUM,   KC_QUOT, KC_4   , KC_5   , KC_6   , KC_EQL ,        _______, _______, KC_RCTL, KC_RSFT, KC_LALT, KC_RGUI,
  _______,    KC_GRV , KC_1   , KC_2   , KC_3   , KC_BSLS,        _______, _______, _______, _______, _______, _______, _______, _______,
              _______, KC_0   , KC_DOT , KC_MINS,        _______, _______, _______, _______
 ),
@@ -105,7 +105,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
             _______, KC_LPRN, KC_RPRN, KC_UNDS,        _______, _______, _______, _______
 ),
  [FUN] = LAYOUT(
-  _______,  _______, _______, _______, _______, _______,                   _______, _______, _______, _______, _______, QK_BOOT,
+  _______,  _______, KC_F10, KC_F11, KC_F12, _______,                   _______, _______, _______, _______, _______, QK_BOOT,
   _______, KC_F12 , KC_F7  , KC_F8  , KC_F9  , KC_PSCR,        _______, _______, _______, _______, _______, _______,
   _______, KC_F11 , KC_F4  , KC_F5  , KC_F6  , _______,        _______, KC_RCTL, KC_RSFT, KC_LALT, KC_RGUI, _______,
   _______,  KC_F10 , KC_F1  , KC_F2  , KC_F3  , _______, _______, _______,  _______,  _______, _______, _______, _______, _______,
