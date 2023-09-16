57b7af6cc1782d7c7d896111a287abcf1a499edd
carakesa <carakesa@gmail.com>
2023-09-15 22:15:27 -0400

    start work on user space and features

---
+++ users/shanec80/config.h
@@ -0,0 +1,33 @@
+/* This is the c configuration file for the keymap Copyright 2023 'shanec80' Copyright 2023
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 2 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#pragma once
+
+//#define USE_MATRIX_I2C
+
+/* Select hand configuration */
+#define MASTER_LEFT
+
+#define SPLIT_USB_TIMEOUT 3000
+#define BOTH_SHIFTS_TURNS_ON_CAPS_WORD
+#define CAPS_WORD_INVERT_ON_SHIFT
+#define QUICK_TAP_TERM 200
+#define TAPPING_TERM 200
+
+//#define HOLD_ON_OTHER_KEY_PRESS
+
+#define PERMISSIVE_HOLD
+#define OLED_TIMEOUT 120000
+#define OLED_BRIGHTNESS 120
