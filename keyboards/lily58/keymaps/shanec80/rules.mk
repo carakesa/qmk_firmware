ab192f79834f28fec7f2234c97d94427bab4f72f
carakesa <carakesa@gmail.com>
2023-12-23 21:35:17 -0500

    modify rules.mk, extra options, bootmagic

--- keyboards/lily58/keymaps/shanec80/rules.mk
+++ keyboards/lily58/keymaps/shanec80/rules.mk
@@ -3,7 +3,7 @@
 #   the appropriate keymap folder that will get included automatically
 #
 WPM_ENABLE = yes
-BOOTMAGIC_ENABLE = no       # Enable Bootmagic Lite
+BOOTMAGIC_ENABLE = yes       # Enable Bootmagic Lite
 MOUSEKEY_ENABLE = yes        # Mouse keys(+4700)
 EXTRAKEY_ENABLE = yes        # Audio control and System control(+450)
 CONSOLE_ENABLE = yes         # Console for debug(+400)
@@ -14,7 +14,7 @@ MIDI_ENABLE = no            # MIDI controls
 AUDIO_ENABLE = no           # Audio output on port C6
 UNICODE_ENABLE = no         # Unicode
 BLUETOOTH_ENABLE = no       # Enable Bluetooth with the Adafruit EZ-Key HID
-RGBLIGHT_ENABLE = no       # Enable WS2812 RGB underlight.
+RGBLIGHT_ENABLE = yes       # Enable WS2812 RGB underlight.
 SWAP_HANDS_ENABLE = no      # Enable one-hand typing
 OLED_ENABLE= yes     # OLED display
 CAPS_WORD_ENABLE = yes
