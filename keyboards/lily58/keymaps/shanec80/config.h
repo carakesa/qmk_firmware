95a9a88814bbeab3f21118be3c09995ae9435ab0
ShaneC80 <53915083+carakesa@users.noreply.github.com>
2025-08-26 21:09:12 -0400

    rebase updates

---
+++ keyboards/lily58/keymaps/shanec80/config.h
@@ -0,0 +1,41 @@
+/* This is the c configuration file for the keymap Copyright 2023 <carakesa@gmail.com> Copyright 2023
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 2 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#pragma once
+
+//#define USE_MATRIX_I2C
+#define LAYER_LOCK_IDLE_TIMEOUT 60000  // Turn off after 60 seconds
+
+/* Select hand configuration */
+#define MASTER_LEFT
+#define SPLIT_USB_TIMEOUT 3000
+#define BOTH_SHIFTS_TURNS_ON_CAPS_WORD
+#define CAPS_WORD_INVERT_ON_SHIFT
+
+//#define HOLD_ON_OTHER_KEY_PRESS
+
+#define QUICK_TAP_TERM 80
+#define TAPPING_TERM 250
+#define TAPPING_TOGGLE 2
+
+//#define HOLD_ON_OTHER_KEY_PRESS
+#define PERMISSIVE_HOLD
+#define OLED_TIMEOUT 90000
+#define OLED_FADE_OUT 45000
+#define OLED_BRIGHTNESS 100
+#define OLED_FADE_OUT_INTERVAL 8
+#define SPLIT_LED_STATE_ENABLE
+#define SPLIT_LED_STATE_ENABLE
+#define SPLIT_WPM_ENABLE //Enable WPM across split keyboards
